e3dae575f37b304df87de50cc758d8fab08a82b2
Ayaz <ayaz@ssvlabs.io>
2025-09-27 05:55:55 +0500

    WIP: disable real proofs

--- validity/src/env.rs
+++ validity/src/env.rs
@@ -128,7 +128,7 @@ pub fn read_proposer_env() -> Result<EnvironmentConfig> {
         // range_proof_interval: get_env_var("RANGE_PROOF_INTERVAL", Some(1800))?, // TODO: revert
         range_proof_interval: 600,
         submission_interval: 600,
-        mock: mock,
+        mock: true, // TODO: WIP
 
         max_concurrent_witness_gen: get_env_var("MAX_CONCURRENT_WITNESS_GEN", Some(1))?,
         max_concurrent_proof_requests: get_env_var("MAX_CONCURRENT_PROOF_REQUESTS", Some(1))?,

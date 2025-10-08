33ec0fb0fd1bff56ae0e7c28600f68582e91e7c8
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-10-08 13:41:53 +0100

    Enable real mode and set range to 100

--- validity/src/env.rs
+++ validity/src/env.rs
@@ -132,9 +132,9 @@ pub fn read_proposer_env() -> Result<EnvironmentConfig> {
         dgf_address: get_env_var("DGF_ADDRESS", Some(Address::ZERO))?,
 
         // range_proof_interval: get_env_var("RANGE_PROOF_INTERVAL", Some(1800))?, // TODO: revert
-        range_proof_interval: 600,
-        submission_interval: 600,
-        mock: true, // TODO: WIP
+        range_proof_interval: 100,
+        submission_interval: 100,
+        mock: mock, // TODO: WIP
 
         max_concurrent_witness_gen: get_env_var("MAX_CONCURRENT_WITNESS_GEN", Some(1))?,
         max_concurrent_proof_requests: get_env_var("MAX_CONCURRENT_PROOF_REQUESTS", Some(1))?,

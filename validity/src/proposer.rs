0bcb3ba836a2780d5fe24a8e7c0d6d2c5e77827f
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-10-09 17:57:37 +0100

    Enable real proof with aggregation

--- validity/src/proposer.rs
+++ validity/src/proposer.rs
@@ -734,7 +734,7 @@ where
 
         // If aggregation is enabled and allowed by single-shot counter, prefer an unrequested aggregation proof.
         let consider_agg = self.requester_config.enable_aggregation
-            && (!self.requester_config.single_shot || self.agg_requests_sent < 1);
+            && (!self.requester_config.single_shot || self.agg_requests_sent < 2);
 
         let unreq_agg_request = if consider_agg {
             self

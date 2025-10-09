b09cd7ea783164a3d0d3fb152d9f33cd9ab9d76a
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-10-09 17:31:04 +0100

    Enable real mode with aggregation

--- validity/src/proposer.rs
+++ validity/src/proposer.rs
@@ -787,7 +787,7 @@ where
         }
 
         // Respect single-shot counter for range requests as well
-        let consider_range = !self.requester_config.single_shot || self.range_requests_sent < 1;
+        let consider_range = !self.requester_config.single_shot || self.range_requests_sent < 3;
 
         let unreq_range_request = if consider_range {
             self

2ab4ccf4a59fa75651904a14eb0cf0a0ea65b784
MatheusFranco99 <48058141+MatheusFranco99@users.noreply.github.com>
2025-10-09 11:59:16 +0100

    enable real mode with aggregation proofs

--- validity/src/env.rs
+++ validity/src/env.rs
@@ -139,7 +139,7 @@ pub fn read_proposer_env() -> Result<EnvironmentConfig> {
         // range_proof_interval: get_env_var("RANGE_PROOF_INTERVAL", Some(1800))?, // TODO: revert
         range_proof_interval: 300,
         submission_interval: 300,
-        mock: true, // TODO: WIP
+        mock: false, // TODO: WIP
 
         max_concurrent_witness_gen: get_env_var("MAX_CONCURRENT_WITNESS_GEN", Some(1))?,
         max_concurrent_proof_requests: get_env_var("MAX_CONCURRENT_PROOF_REQUESTS", Some(1))?,
@@ -159,7 +159,7 @@ pub fn read_proposer_env() -> Result<EnvironmentConfig> {
         agg_cycle_limit: get_env_var("AGG_CYCLE_LIMIT", Some(1_000_000_000_000))?,     // 1 trillion
         agg_gas_limit: get_env_var("AGG_GAS_LIMIT", Some(1_000_000_000_000))?,         // 1 trillion
         min_l2_block: 520000, // get_env_var("MIN_L2_BLOCK", Some(0))?,
-        enable_aggregation: false, //get_env_var("ENABLE_AGGREGATION", Some(true))?,
+        enable_aggregation: true, //get_env_var("ENABLE_AGGREGATION", Some(true))?,
         // Optional: HTTP endpoint for the shared publisher service.
         // Example: http://localhost:8081/v1/proofs/op-succinct
         publisher_url: env::var("SHARED_PUBLISHER_URL").ok().and_then(|s| Url::parse(&s).ok()),

a9e54a98016a0d1c2bb3e31993b427f943d58c1f
Ayaz <ayaz@ssvlabs.io>
2025-10-14 12:45:18 +0700

    Set min_l2_block: 600000

--- validity/src/env.rs
+++ validity/src/env.rs
@@ -171,7 +171,7 @@ pub fn read_proposer_env() -> Result<EnvironmentConfig> {
         range_gas_limit: get_env_var("RANGE_GAS_LIMIT", Some(1_000_000_000_000))?,     // 1 trillion
         agg_cycle_limit: get_env_var("AGG_CYCLE_LIMIT", Some(1_000_000_000_000))?,     // 1 trillion
         agg_gas_limit: get_env_var("AGG_GAS_LIMIT", Some(1_000_000_000_000))?,         // 1 trillion
-        min_l2_block: 521500, // get_env_var("MIN_L2_BLOCK", Some(0))?,
+        min_l2_block: 600000, // get_env_var("MIN_L2_BLOCK", Some(0))?,
         enable_aggregation: true, // get_env_var("ENABLE_AGGREGATION", Some(true))?,
         single_shot: false, // get_env_var("SINGLE_SHOT", Some(false))?,
         // Optional: HTTP endpoint for the shared publisher service.
